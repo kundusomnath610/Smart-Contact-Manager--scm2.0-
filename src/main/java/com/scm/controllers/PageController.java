a8c81f1f3dabc6704f1ad0cb0c0d1b3b24901e64
Somnath Kundu <kundusomnath610@gmail.com>
2024-05-03 14:29:22 +0530

    Initial Commit

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -1,13 +1,18 @@
 package com.scm.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
 
     @RequestMapping("/home")
-    public String home() {
+    public String home(Model model) {
+        // Sending Data to view....
+        model.addAttribute("name", "Substring");
+        model.addAttribute("somnath", "kundu");
+        model.addAttribute("GithubRepo", "https://github.com/kundusomnath610");
         System.out.println("Home page handeler");
         return "home";
     }
