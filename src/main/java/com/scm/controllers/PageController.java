81f6d58275723037a6ce9a8810ec8f1e27f39265
Somnath Kundu <kundusomnath610@gmail.com>
2024-05-17 13:16:04 +0530

    initial commit

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -3,6 +3,9 @@ package com.scm.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class PageController {
@@ -32,4 +35,27 @@ public class PageController {
         return "service";
     }
 
+    // Contact Page.. 
+    @GetMapping("/contact")
+    public String contact() {
+        System.out.println("This is contact page");
+        return new String("contact");
+    }
+
+    //Login Page
+    @GetMapping("/login")
+    public String login() {
+        System.out.println("This is log in page");
+        return new String("login");
+    }
+
+    //register..
+    @GetMapping("/register")
+    public String register() {
+        System.out.println("This is register page");
+        return new String("register");
+    }
+    
+    
+    
 }
