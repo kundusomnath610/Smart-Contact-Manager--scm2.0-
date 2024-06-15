7d7be5c68088c94d66dafb70f9b95ee0b17240d1
Somnath Kundu <kundusomnath610@gmail.com>
2024-06-16 03:08:48 +0530

    initial commit

--- src/main/java/com/scm/entities/SocialLink.java
+++ src/main/java/com/scm/entities/SocialLink.java
@@ -1,6 +1,9 @@
 package com.scm.entities;
 
+import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
@@ -18,6 +21,7 @@ import lombok.Setter;
 
 public class SocialLink {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String link;
     private String title;
