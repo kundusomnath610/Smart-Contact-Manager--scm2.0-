4a4e9416e3454e44d07acc2c41e553d283089991
Somnath Kundu <kundusomnath610@gmail.com>
2024-08-25 03:38:55 +0530

    initial commit

--- src/main/resources/static/js/contacts.js
+++ src/main/resources/static/js/contacts.js
@@ -5,8 +5,7 @@ const viewContactModal = document.getElementById("view_contact_modal");
 const options = {
     placement: 'bottom-right',
     backdrop: 'dynamic',
-    backdropClasses:
-        'bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-40',
+    backdropClasses: 'bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-40',
     closable: true,
     onHide: () => {
         console.log('modal is hidden');
@@ -19,8 +18,29 @@ const options = {
     },
 };
 
-const contactModal = new Modal(viewContactModal, options, instacnceOptions);
+// Initialize the modal instance with the correct options
+const contactModal = new Modal(viewContactModal, options);
 
 function openContactModal() {
-    contactModal.Show();
+    contactModal.show();  // Use show() for View the contact
+}
+
+function closeContactModal() {
+    contactModal.hide(); // use hide to close the view of contact modal..
+}
+
+ async function loadContactData(id) {
+    console.log(id);
+
+    try {
+        const data = await(
+            await fetch(`http://127.0.0.1:8081/api/contacts/${id}`)
+        ).json();
+        console.log(data);
+        document.querySelector("#contact_name").innerHTML = data.name;
+        document.querySelector("#contact_email").innerHTML = data.email;
+        openContactModal();
+    } catch (error) {
+        console.log("Error :" + error);
+    }
 }
